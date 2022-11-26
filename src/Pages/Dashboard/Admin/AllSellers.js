360c12b1a04143d5df86a03974fd719082adda10
unknown <biplob2358@gmail.com>
2022-11-26 12:10:00 +0600

    seller route added

--- src/Pages/Dashboard/Admin/AllSellers.js
+++ src/Pages/Dashboard/Admin/AllSellers.js
@@ -14,9 +14,37 @@ const AllSellers = () => {
     return (
         <div>
             <h2>All Buyers</h2>
-            {
-                sellers.map(seller => <p>{seller.name}</p>)
-            }
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Status</th>
+                            <th>Action</th>
+
+
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            sellers.map((seller, i) => <tr key={seller._id}>
+                                <th>{i + 1}</th>
+                                <td>{seller.name}</td>
+                                <td>{seller.email}</td>
+                                <td>{seller.role}</td>
+                                <td> <button className="btn  shadow-lg bg-green-400 btn-xs">make verified</button> </td>
+                                <td> <button className="btn  shadow-lg bg-red-400 btn-xs">Delete</button> </td>
+
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
